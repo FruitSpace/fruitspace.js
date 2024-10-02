e06e38157a2b9f9c8c8f3cfa1e0498903f47518f
M41den <38406005+m41denx@users.noreply.github.com>
2024-10-02 14:26:04 +0300

    kildoom dumb

--- dist/fruitspaceclient/client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fruitspaceClient = void 0;
-class fruitspaceClient {
-    constructor() { }
-}
-exports.fruitspaceClient = fruitspaceClient;
